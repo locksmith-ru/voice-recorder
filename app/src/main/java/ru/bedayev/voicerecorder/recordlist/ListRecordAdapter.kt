122bdbde73976cf63a383dd3bf1bacb38311a360
Sergey Bedaev <serge_sargon@mail.ru>
2023-09-08 23:54:49 +1000

    Внедрение composable в код ListRecordAdapter

--- app/src/main/java/ru/bedayev/voicerecorder/recordlist/ListRecordAdapter.kt
+++ app/src/main/java/ru/bedayev/voicerecorder/recordlist/ListRecordAdapter.kt
@@ -3,15 +3,11 @@ package ru.bedayev.voicerecorder.recordlist
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import ru.bedayev.voicerecorder.R
 import ru.bedayev.voicerecorder.database.RecordingItem
 import ru.bedayev.voicerecorder.databinding.ListItemRecordBinding
-import timber.log.Timber
-import java.io.File
-import java.util.Locale
-import java.util.concurrent.TimeUnit
+import ru.bedayev.voicerecorder.ui.ListRecordCard
+import ru.bedayev.voicerecorder.ui.theme.VoiceRecorderTheme
 import kotlin.text.*
 
 class ListRecordAdapter(
@@ -40,41 +36,15 @@ class ListRecordAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item: RecordingItem? = data.getOrNull(position)
         item?.let { record ->
-            val itemDuration: Long = record.length
-            val minutes: Long = TimeUnit.MILLISECONDS.toMinutes(itemDuration)
-            val seconds: Long = (TimeUnit.MILLISECONDS.toSeconds(itemDuration) -
-                    TimeUnit.MINUTES.toSeconds(minutes))
-            with(holder.binding) {
-                fileNameText.text = record.name
-                fileLengthText.text = String
-                    .format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
-                cardView.setOnClickListener {
-                    val filePath = record.filePath
-                    val file = File(filePath)
-                    if (file.exists()) {
-                        try {
-                            onItemClicked(filePath)
-                        } catch (e: Exception) {
-                            Timber.e("onItemClick an error has occurred: ${e.message}", e)
-                        }
-                    } else {
-                        Toast.makeText(
-                            cardView.context,
-                            R.string.file_is_not_exist_text,
-                            Toast.LENGTH_SHORT
-                        )
-                            .show()
-                    }
-                }
-                cardView.setOnLongClickListener {
-                    onRemoveItem(record.id, record.filePath)
-                    false
-                }
-                removeButton.setOnClickListener{
-                    onRemoveItem(record.id, record.filePath)
+            holder.binding.composeView.setContent {
+                VoiceRecorderTheme {
+                    ListRecordCard(
+                        recordingItem = record,
+                        onItemClick = onItemClicked,
+                        onRemove = onRemoveItem
+                    )
                 }
             }
-
         }
     }
 
